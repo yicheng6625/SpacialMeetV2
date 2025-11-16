c1f53b576fc4827531e5f922a5061f1c0b02f9a0
Jitto Joseph <jitto.joseph67@gmail.com>
2025-11-16 20:02:36 +0530

    Add WebSocket configuration

---
+++ apps/backend-springboot/src/main/java/com/spatialmeet/WebSocketConfig.java
@@ -0,0 +1,22 @@
+package com.spatialmeet;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final GameWebSocketHandler gameWebSocketHandler;
+
+    public WebSocketConfig(GameWebSocketHandler gameWebSocketHandler) {
+        this.gameWebSocketHandler = gameWebSocketHandler;
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(gameWebSocketHandler, "/ws").setAllowedOrigins("*");
+    }
+}
