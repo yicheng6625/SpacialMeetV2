5c3ce9eb29ddc4bcfab4f8d7ed4d5b6e28168620
Jitto Joseph <jitto.joseph67@gmail.com>
2025-11-16 20:02:21 +0530

    Add main application class

---
+++ apps/backend-springboot/src/main/java/com/spatialmeet/SpatialMeetApplication.java
@@ -0,0 +1,13 @@
+package com.spatialmeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpatialMeetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpatialMeetApplication.class, args);
+    }
+
+}
